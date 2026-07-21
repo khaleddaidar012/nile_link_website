3eba0358d814fe6541f2153904a20de56af8b4dd
khaleddaidar012 <khaleddaidar012@gmail.com>
2026-07-21 20:28:53 +0300

    Fix build error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,24 +1,22 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI
-
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI is not defined in .env.local")
-}
-
 let cached = global as unknown as { mongoose?: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null } }
 
-if (!cached.mongoose) {
-  cached.mongoose = { conn: null, promise: null }
-}
-
 export async function connectDB() {
-  if (cached.mongoose!.conn) return cached.mongoose!.conn
-
-  if (!cached.mongoose!.promise) {
-    cached.mongoose!.promise = mongoose.connect(MONGODB_URI!)
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined in environment variables")
   }
 
-  cached.mongoose!.conn = await cached.mongoose!.promise
-  return cached.mongoose!.conn
+  if (!cached.mongoose) {
+    cached.mongoose = { conn: null, promise: null }
+  }
+
+  if (cached.mongoose.conn) return cached.mongoose.conn
+
+  if (!cached.mongoose.promise) {
+    cached.mongoose.promise = mongoose.connect(process.env.MONGODB_URI)
+  }
+
+  cached.mongoose.conn = await cached.mongoose.promise
+  return cached.mongoose.conn
 }
